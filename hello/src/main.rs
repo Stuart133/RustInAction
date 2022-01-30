497bf36071596e557fb505421fc88fbb2604f408
Stuart Reid <stuart@circleci.com>
2022-01-30 15:39:22 +0000

    CSV parsing example

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+fn greet_world() {
     println!("Hello, world!");
+    let southernm_germany = "Grüß Gott!";
+    let japan = "ハロー・ワールド";
+    let regions = [southernm_germany, japan];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
+
+fn main() {
+    greet_world();
 }
