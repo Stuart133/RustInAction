4e98019b1f8cf293433a4b91965f847507190b7e
Stuart Reid <stuart@circleci.com>
2022-02-07 18:18:12 +0000

    Define file struct

--- files/src/main.rs
+++ files/src/main.rs
@@ -1,6 +1,10 @@
 #![allow(unused_variables)]
 
-type File = String;
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
 
 fn open(f: &mut File) -> bool {
     true
@@ -16,7 +20,17 @@ fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
 }
 
 fn main() {
-    let mut f1 = File::from("f1.txt");
+    let mut f1 = File {
+        name: String::from("f1.txt"),
+        data: Vec::new(),
+    };
+
+    let f1_name = &f1.name;
+    let f1_length = &f1.data.len();
+
+    println!("{:?}", f1);
+    println!("{} is {} bytes long", f1_name, f1_length);
+
     open(&mut f1);
     // read(f1, vec![]);
     close(&mut f1);
