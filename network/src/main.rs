d63cc0a8d4f3ee2b70e6c8e6d59032dfbf944ce9
Stuart Reid <stuart1334@gmail.com>
2022-02-26 11:37:14 +0000

    Use raw TCP stream

--- network/src/main.rs
+++ network/src/main.rs
@@ -1,12 +1,21 @@
-use std::error::Error;
-use reqwest;
+use std::io::prelude::*;
+use std::net::TcpStream;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let url = "http://www.rustinaction.com";
-    let mut response = reqwest::get(url)?;
+fn main() -> std::io::Result<()> {
+    let host = "www.rustinaction.com:80";
 
-    let content = response.text()?;
-    print!("{}", content);
+    let mut conn = TcpStream::connect(host)?;
+
+    conn.write_all(b"GET / HTTP/1.0")?;
+    conn.write_all(b"\r\n")?;
+
+    conn.write_all(b"Host: www.rustinaction.com")?;
+    conn.write_all(b"\r\n\r\n")?;
+
+    std::io::copy(
+        &mut conn,
+        &mut std::io::stdout()
+    )?;
 
     Ok(())
 }
